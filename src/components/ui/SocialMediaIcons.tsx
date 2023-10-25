791a85c76237850e87b6c059881494d33445a725
Salem Alnahdi <alnahdi4b@gmail.com>
2023-10-25 11:54:48 +0300

    Update SocialMediaIcon.tsx
    Add social media icons and links to  SocialMediaIcon.tsx Component

--- src/components/ui/SocialMediaIcons.tsx
+++ src/components/ui/SocialMediaIcons.tsx
@@ -7,11 +7,11 @@ export default function SocialMediaIcons() {
   return (
     <div className="hidden md:flex gap-4 items-end justify-center  ">
       {/* Adjust the size using Tailwind CSS classes */}
-      <FiYoutube className="text-xl" />
-      <FaInstagram className="text-xl" />
-      <SlSocialLinkedin className="text-xl " />
-      <FaXTwitter className="text-xl" />
-      <FiFacebook className="text-xl" />
+      <a title="Youtube"  href="https://www.youtube.com/@oiscb/featured" target="_blank" rel="noopener"><FiYoutube className="text-xl" /></a>
+      <a title="instagram" href="https://www.instagram.com/oiscb/" target="_blank" rel="noopener"><FaInstagram className="text-xl" /></a>
+      <a title="linkedin" href="https://ye.linkedin.com/in/%D9%85%D8%B9%D9%87%D8%AF-%D8%A7%D9%84%D8%B9%D9%85%D8%B1%D8%A7%D9%86-%D9%84%D9%84%D8%AF%D8%B1%D8%A7%D8%B3%D8%A7%D8%AA-%D9%88%D8%A8%D9%86%D8%A7%D8%A1-%D8%A7%D9%84%D9%82%D8%AF%D8%B1%D8%A7%D8%AA-oiscb-8b02b61a6" target="_blank" rel="noopener"><SlSocialLinkedin className="text-xl " /></a>
+      <a title="Twitter" href="https://twitter.com/OISCB1" target="_blank" rel="noopener"><FaXTwitter className="text-xl"  /></a>
+      <a title="Facebook" href="https://www.facebook.com/OISCB" target="_blank" rel="noopener"><FiFacebook className="text-xl" /></a>
     </div>
   );
 }
